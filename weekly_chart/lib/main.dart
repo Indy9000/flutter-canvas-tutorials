1eba3ded6885de29d28ca3486e452ca3cad2a1ca
Indy9000 <indy9000@gmail.com>
2020-11-28 10:28:22 +0000

    experiment

---
+++ weekly_chart/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'my-painter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Week Chart',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyPainter(),
+    );
+  }
+}

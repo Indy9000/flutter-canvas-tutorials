5c116d636a3155a6c3577261dd25ab1e6845999c
Indy9000 <indy9000@gmail.com>
2020-12-20 19:20:08 +0000

    as per vid

---
+++ christmas_card/android/app/src/main/kotlin/com/example/christmas_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.christmas_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ christmas_card/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:christmas_card/greeting_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: GreetingWidget(),
+    );
+  }
+}
